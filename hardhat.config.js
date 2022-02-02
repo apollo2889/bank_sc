bb3f17a4d222cb64fdbaebd5193164e508a95a80
apollo2889 <apollo1205@hotmail.com>
2022-02-02 16:05:16 +0800

    refactoring env

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: `${process.env.ALCHEMY_RINKEBY_URL}`,
+      url: `${process.env.ALCHEMY_ROPSTEN_URL}`,
       accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
     } 
   }
